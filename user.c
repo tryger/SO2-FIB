59b9fe39a9ac8c08dbd5fa2f51eb4d8fc72e45d5
tryger <tryger>
2018-09-18 13:35:07 +0200

    Practica 0

---
+++ user.c
@@ -0,0 +1,52 @@
+#include <libc.h>
+
+char buff[24];
+
+int pid;
+
+
+int add2(int, int);
+
+long inner( long n)
+{
+	int  i;
+	long suma;
+	
+	suma = 0;
+	for (i=0; i<n; i++) suma = suma + i;
+
+	return suma;
+}
+
+long outer(long n)
+{
+	int i;
+	long acum;
+
+	acum = 0;
+	for (i=0; i<n; i++) acum = acum + inner(i);
+
+	return acum;
+}
+
+int __attribute__ ((__section__(".text.main")))
+  main(void)
+{
+    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
+     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+
+	long count, acum;
+
+	count = 75;
+	acum = outer(count);
+
+	count = add2(2, acum);
+
+	while (1);
+
+  	return 0;
+}
+int add(int par1, int par2)
+{
+	return par1 + par2;
+}
